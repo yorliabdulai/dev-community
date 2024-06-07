ae495a08a0b588d731dcc02fc36b97187e890862
yorliabdulai <yorliabdulai1@gmail.com>
2024-06-07 08:35:58 +0000

    Add User model with password encryption

---
+++ backend/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isMentor: { type: Boolean, default: false },
+  isAdmin: { type: Boolean, default: false },
+  isManager: { type: Boolean, default: false },
+  isPremium: { type: Boolean, default: false },
+}, { timestamps: true });
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
