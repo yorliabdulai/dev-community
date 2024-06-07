94d3b0e274326e0e8c4f03612d22b06ca033ced2
yorliabdulai <yorliabdulai1@gmail.com>
2024-06-07 13:19:33 +0000

    Add mongoose options for index creation and find-and-modify operations

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,8 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 }).then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }).catch((err) => console.log(err));
